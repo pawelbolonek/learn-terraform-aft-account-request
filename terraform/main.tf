a6cdb0394daac7df643fd99bae73ffae2cffa22c
Paweł Bolonek <e-plbk@gft.com>
2022-06-24 15:32:31 +0200

    Adding account4 request

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "account4" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "pawel.bolonek.job+amazon+account4@gmail.com"
+    AccountName               = "account4"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "pawel.bolonek.job+amazon+account4@gmail.com"
+    SSOUserFirstName          = "Paweł"
+    SSOUserLastName           = "Bolonek"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "account4"
+}
