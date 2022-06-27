c8c0897550e2b88a311881231af2118345fab28a
Paweł Bolonek <e-plbk@gft.com>
2022-06-27 08:59:36 +0200

    Removing account 3 via terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,30 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "pawel.bolonek.job+amazon+account3@gmail.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "pawel.bolonek.job+amazon+account3@gmail.com"
-    SSOUserFirstName          = "Paweł"
-    SSOUserLastName           = "Bolonek"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
+#module "sandbox" {
+#  source = "./modules/aft-account-request"
+#
+#  control_tower_parameters = {
+#    AccountEmail              = "pawel.bolonek.job+amazon+account3@gmail.com"
+#    AccountName               = "sandbox-aft"
+#    ManagedOrganizationalUnit = "Sandbox"
+#    SSOUserEmail              = "pawel.bolonek.job+amazon+account3@gmail.com"
+#    SSOUserFirstName          = "Paweł"
+#    SSOUserLastName           = "Bolonek"
+#  }
+#
+#  account_tags = {
+#    "Learn Tutorial" = "AFT"
+#  }
+#
+#  change_management_parameters = {
+#    change_requested_by = "HashiCorp Learn"
+#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#  }
+#
+#  custom_fields = {
+#    group = "non-prod"
+#  }
+#
+#  account_customizations_name = "sandbox"
+#}
 
 module "account4" {
   source = "./modules/aft-account-request"
